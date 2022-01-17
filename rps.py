a97cbc2737f44f0ee8505c2f8dbb0e7b86f4121d
Christian Kimura <christiankimura94@gmail.com>
2022-01-17 16:34:48 -0600

    simplifying tied into one statement

--- rps.py
+++ rps.py
@@ -14,15 +14,16 @@ computer_choice = random.choice(CHOICES)
 whatThrow = f"\nYou threw '{user_choice}', the computer threw '{computer_choice}'"
 
 #Starting the function to determine winner based on if statements
+#IF TIED
+if user_choice == computer_choice:
+    print(whatThrow)
+    print(tied)
 #IF ROCK
-if user_choice == "rock":
+elif user_choice == "rock":
     if computer_choice == "scissors":
         print(whatThrow)
         print("Rock crushes scissors")
         print(winner)
-    elif computer_choice == "rock":
-        print(whatThrow)
-        print(tied)
     else:
         print(whatThrow)
         print("Paper covers Rock")
@@ -37,9 +38,6 @@ elif user_choice == "scissors":
         print(whatThrow)
         print("Rock crushes Scissors")
         print(loser)
-    else:
-        print(whatThrow)
-        print(tied)
 #IF PAPER
 elif user_choice == "paper":
     if computer_choice == "rock":
@@ -50,8 +48,5 @@ elif user_choice == "paper":
         print(whatThrow)
         print("Scissors cuts Paper")
         print(loser)
-    else:
-        print(whatThrow)
-        print(tied)
 else:
     print(f"\n You typed '{user_choice}' which isn't a valid throw")
