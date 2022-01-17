4f14a0d9430bf09095be12ed1456f87a8663c400
Christian Kimura <christiankimura94@gmail.com>
2022-01-17 14:07:10 -0600

    adding functionality \nDetermines prompts user for RPS then randomly decides computer throw then through a series of if elif statements determines winner.

--- rps.py
+++ rps.py
@@ -2,16 +2,56 @@
 #Rock Paper Scissors exercise using python
 import random
 
+#Setting up the 3 options
 CHOICES = ["rock", "paper", "scissors"]
 
-print("Make your throw")
-
+#Establishing all other variables in order to shorten code
+loser = "\nYou Lose :("
+winner = "\nYOU WIN!"
+tied = "\nIt's a Tie"
 user_choice = input("   Type rock, paper, or scissors\n")
+computer_choice = random.choice(CHOICES)
+whatThrow = f"\nYou threw '{user_choice}', the computer threw '{computer_choice}'"
 
-if user_choice in CHOICES:
-    computer_choice = random.choice(CHOICES)
-    print(
-        f"\nYou threw '{user_choice}', the computer threw '{computer_choice}'"
-    )
+#Starting the function to determine winner based on if statements
+#IF ROCK
+if user_choice == "rock":
+    if computer_choice == "scissors":
+        print(whatThrow)
+        print("Rock crushes scissors")
+        print(winner)
+    elif computer_choice == "rock":
+        print(whatThrow)
+        print(tied)
+    else:
+        print(whatThrow)
+        print("Paper covers Rock")
+        print(loser)
+#IF SCISSORS
+elif user_choice == "scissors":
+    if computer_choice == "paper":
+        print(whatThrow)
+        print("Scissors cuts Paper")
+        print(winner)
+    elif computer_choice == "rock":
+        print(whatThrow)
+        print("Rock crushes Scissors")
+        print(loser)
+    else:
+        print(whatThrow)
+        print(tied)
+#IF PAPER
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print(whatThrow)
+        print("Paper covers Rock")
+        print(winner)
+    elif computer_choice == "scissors":
+        print(whatThrow)
+        print("Scissors cuts Paper")
+        print(loser)
+    else:
+        print(whatThrow)
+        print(tied)
 else:
     print(f"\n You typed '{user_choice}' which isn't a valid throw")
